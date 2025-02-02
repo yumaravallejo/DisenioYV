2e88f2a955ddd9713a53a6144de329e42dc12a79
Yumara Vallejo <145961485+yumaravallejo@users.noreply.github.com>
2025-02-02 12:26:51 +0100

    findea

--- Findea/js/script.js
+++ Findea/js/script.js
@@ -2,102 +2,86 @@ const pinterestImg = document.getElementById("pinterest"),
   facebookImg = document.getElementById("facebook"),
   instagramImg = document.getElementById("instagram"),
   emailImg = document.getElementById("email"),
-  btnLeft = document.querySelector(".btn-left"),
-  btnRight = document.querySelector(".btn-right"),
-  positioner = document.querySelectorAll(".position");
+  positioner = document.querySelectorAll(".position"),
   slider = document.querySelector("#slider"),
   body = document.body,
-  hamburgerCheckbox = document.getElementById('hamburger'),
+  hamburgerCheckbox = document.getElementById("hamburger"),
   sliderSection = document.querySelectorAll(".slider-section"); //Esto devuelve una nodelist con los 3 section
 
-pinterestImg.addEventListener("mouseenter", () => {
-  pinterestImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/pinterest-a.svg";
-});
+$("#pinterest")
+  .on("mouseenter", () => {
+    pinterestImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/pinterest-a.svg";
+  })
+  .on("mouseleave", () => {
+    pinterestImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/pinterest-n.svg";
+  });
 
-pinterestImg.addEventListener("mouseleave", () => {
-  pinterestImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/pinterest-n.svg";
-});
+$("#facebook")
+  .on("mouseenter", () => {
+    facebookImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/facebook-a.svg";
+  })
+  .on("mouseleave", () => {
+    facebookImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/facebook-n.svg";
+  });
 
-facebookImg.addEventListener("mouseenter", () => {
-  facebookImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/facebook-a.svg";
-});
+$("#instagram")
+  .on("mouseenter", () => {
+    instagramImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/instagram-a.svg";
+  })
+  .on("mouseleave", () => {
+    instagramImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/instagram-n.svg";
+  });
 
-facebookImg.addEventListener("mouseleave", () => {
-  facebookImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/facebook-n.svg";
-});
-
-instagramImg.addEventListener("mouseenter", () => {
-  instagramImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/instagram-a.svg";
-});
-
-instagramImg.addEventListener("mouseleave", () => {
-  instagramImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/instagram-n.svg";
-});
-
-emailImg.addEventListener("mouseenter", () => {
-  emailImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/mail-a.svg";
-});
-
-emailImg.addEventListener("mouseleave", () => {
-  emailImg.src =
-    "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/mail-n.svg";
-});
-
-btnLeft.addEventListener("click", (e) => moveToLeft());
-btnRight.addEventListener("click", (e) => moveToRight());
+$("#email")
+  .on("mouseenter", () => {
+    emailImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/mail-a.svg";
+  })
+  .on("mouseleave", () => {
+    emailImg.src =
+      "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea/mail-n.svg";
+  });
 
 let operacion = 0,
   counter = 0,
   widthImg = 100 / sliderSection.length;
 
-function moveToRight() {
-  if (counter >= sliderSection.length - 1) {
-    counter = 0;
-    operacion = 0;
-    slider.style.transform = `translate(-${operacion}%)`;
-    positioner[0].style.background = "var(--azul-claro)"
-    positioner[2].style.background = "white";
+$(".btn-left").on("click", () => {
+  counter = (counter - 1 + sliderSection.length) % sliderSection.length;
+  operacion = counter * widthImg;
 
-    // slider.style.transition = "none"; Para que no tenga transición en la última
-  } else {
-    counter++;
-    operacion = operacion+widthImg;
-    slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .6s";
-    
-    positioner[counter-1].style.background = "white"
-    positioner[counter].style.background = "var(--azul-claro)"
-  }
-}
-
-function moveToLeft() {
-  counter--;
-
-  if (counter < 0) {
-    counter = sliderSection.length-1;
-
-    operacion = widthImg * (sliderSection.length-1);
-    slider.style.transform = `translate(-${operacion}%)`;
-    // slider.style.transition = "none"; Para que no tenga transición en la última
-    return;
-  }
-  operacion = operacion - widthImg;
   slider.style.transform = `translate(-${operacion}%)`;
   slider.style.transition = "all ease .6s";
+
+  posicionar();
+});
+
+$(".btn-right").on("click", () => {
+  counter = (counter + 1) % sliderSection.length;
+  operacion = counter * widthImg;
+
+  slider.style.transform = `translate(-${operacion}%)`;
+  slider.style.transition = "all ease .6s";
+
+  posicionar();
+});
+
+function posicionar() {
+  positioner.forEach((pos, index) => {
+    pos.style.background = index === counter ? "var(--azul-claro)" : "white";
+  });
 }
 
-//Que no se pueda hacer scroll para que el menú quede como página aparte
-hamburgerCheckbox.addEventListener('change', function() {
+hamburgerCheckbox.on("change", function () {
   if (hamburgerCheckbox.checked) {
-    body.classList.add('no-scroll'); // Bloquea el scroll
+    body.classList.add("no-scroll");
   } else {
-    body.classList.remove('no-scroll'); // Permite el scroll nuevamente
+    body.classList.remove("no-scroll");
   }
 });
