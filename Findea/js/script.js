922e15b04f68e33e196287369381f7dec5e18ce2
Yumara Vallejo <145961485+yumaravallejo@users.noreply.github.com>
2025-02-23 10:41:55 +0100

    findea

--- Findea/js/script.js
+++ Findea/js/script.js
@@ -8,13 +8,22 @@ function changeImageOnHover(id, baseUrl, iconName) {
   $("#" + id)
     .on("mouseenter", () => {
       //Imagen azul
-      $("#" + id).attr("src", `${baseUrl}/${iconName}-a.svg`);
+      $(this).attr("src", `${baseUrl}/${iconName}-a.svg`);
     })
     .on("mouseleave", () => {
       //Imagen negra
-      $("#" + id).attr("src", `${baseUrl}/${iconName}-n.svg`);
+      $(this).attr("src", `${baseUrl}/${iconName}-n.svg`);
     });
 }
+
+$(".menu-toggle").on("click", () => {
+  $(".main-menu").finish.animate(
+    {
+      "left": "0",
+    },
+    "fast"
+  );
+});
 
 const baseUrl = "https://fotos-yvv.s3.us-east-1.amazonaws.com/Iconos-Findea";
 
@@ -23,35 +32,126 @@ changeImageOnHover("facebook", baseUrl, "facebook");
 changeImageOnHover("instagram", baseUrl, "instagram");
 changeImageOnHover("email", baseUrl, "mail");
 
-let operacion = 0,
-  counter = 0,
-  widthImg = 100 / sliderSection.length;
+$(function () {
+  var SliderModule = (function () {
+    var pb = {};
+    pb.elslider = $("#slider");
+    pb.items = {
+      panels: pb.elslider.find(".slider-wrapper > li"),
+    };
 
-$(".btn-left, .btn-left-peq").on("click", () => {
-  counter = (counter - 1 + sliderSection.length) % sliderSection.length;
-  operacion = counter * widthImg;
+    var SliderInterval,
+      currentSlider = 0,
+      lengthSlider = pb.items.panels.length;
 
-  slider.style.transform = `translate(-${operacion}%)`;
-  slider.style.transition = "all ease .6s";
+    pb.init = function () {
+      var loscontroles = "";
+      SliderInit();
 
-  posicionar();
+      // Generar los controles de navegación
+      for (var i = 0; i < lengthSlider; i++) {
+        loscontroles += "<li" + (i === 0 ? ' class="active"' : "") + "></li>";
+      }
+      $("#control-buttons").html(loscontroles);
+
+      // Acción al hacer clic en un control
+      $("#control-buttons li").click(function () {
+        var index = $(this).index();
+        if (currentSlider !== index) {
+          cambiarPanel(index);
+        }
+      });
+
+      $(".slider-wrapper li").on("mouseenter", function () {
+        clearInterval(SliderInterval);
+      });
+
+      $(".slider-wrapper li").on("mouseleave", function () {
+        SliderInit();
+      });
+
+      // Acción al hacer clic en los botones
+      $(".btn-left, .btn-left-peq").on("click", function () {
+        changePanel(-1); // Mover a la izquierda
+      });
+
+      $(".btn-right, .btn-right-peq").on("click", function () {
+        changePanel(1); // Mover a la derecha
+      });
+
+      // Mostrar imagen al hacer hover sobre el control
+      $("#control-buttons li").on("mouseenter", function () {
+        var index = $(this).index();
+        var imgSrc = pb.items.panels.eq(index).find("img").attr("src");
+        $("#preview-img > img").attr("src", imgSrc);
+        $("#preview-img").show();
+      });
+
+      $("#control-buttons li").on("mouseleave", function () {
+        $("#preview-img").hide();
+      });
+    };
+
+    var SliderInit = function () {
+      SliderInterval = setInterval(pb.startSlider, 3000);
+    };
+
+    pb.startSlider = function () {
+      var nextSlider = (currentSlider + 1) % lengthSlider;
+      slideToNext(nextSlider);
+    };
+
+    var slideToNext = function (nextSlider) {
+      var paneles = pb.items.panels;
+      var controles = $("#control-buttons li");
+
+      controles.removeClass("active");
+      controles.eq(nextSlider).addClass("active");
+
+      pb.elslider
+        .find(".slider-wrapper")
+        .finish()
+        .animate(
+          {
+            "margin-left": "-" + nextSlider * 100 + "%",
+          },
+          500
+        );
+
+      currentSlider = nextSlider;
+    };
+
+    var changePanel = function (num) {
+      clearInterval(SliderInterval);
+
+      var nextSlider = (currentSlider + num + lengthSlider) % lengthSlider;
+      slideToNext(nextSlider);
+      SliderInit();
+    };
+
+    var cambiarPanel = function (indice) {
+      clearInterval(SliderInterval);
+
+      var controles = $("#control-buttons li");
+      controles.removeClass("active");
+      controles.eq(indice).addClass("active");
+
+      pb.elslider.find(".slider-wrapper").animate(
+        {
+          "margin-left": "-" + indice * 100 + "%",
+        },
+        500
+      );
+
+      currentSlider = indice;
+      SliderInit();
+    };
+
+    return pb;
+  })();
+
+  SliderModule.init();
 });
-
-$(".btn-right, .btn-right-peq").on("click", () => {
-  counter = (counter + 1) % sliderSection.length;
-  operacion = counter * widthImg;
-
-  slider.style.transform = `translate(-${operacion}%)`;
-  slider.style.transition = "all ease .6s";
-
-  posicionar();
-});
-
-function posicionar() {
-  positioner.forEach((pos, index) => {
-    pos.style.background = index === counter ? "var(--azul-claro)" : "white";
-  });
-}
 
 $("#hamburger").on("change", function () {
   if (hamburgerCheckbox.checked) {
@@ -63,7 +163,7 @@ $("#hamburger").on("change", function () {
 
 $("#atras").on("click", function () {
   window.history.back();
-})
+});
 
 $(".bac-rojo")
   .on("mouseleave", function () {
@@ -81,20 +181,20 @@ $(".bac-azul")
     $(this).css("background-color", "#2B538F");
   });
 
-  $("#flec-select").on("click", function() {
-    var currentTransform = $("#flec-select > img").css("transform");
+$("#flec-select").on("click", function () {
+  var currentTransform = $("#flec-select > img").css("transform");
 
-    if (currentTransform != "matrix(1, 0, 0, 1, 0, 0)") {
-        $("#flec-select > img").css("transform", "rotate(0deg)");
-    } else {
-        $("#flec-select > img").css("transform", "rotate(180deg)");
-    }
-   
-    // Muestra u oculta el menú de la lista
-    $("#todas-listas").slideToggle(200);
+  if (currentTransform != "matrix(1, 0, 0, 1, 0, 0)") {
+    $("#flec-select > img").css("transform", "rotate(0deg)");
+  } else {
+    $("#flec-select > img").css("transform", "rotate(180deg)");
+  }
+
+  // Muestra u oculta el menú de la lista
+  $("#todas-listas").slideToggle(200);
 });
 
 function irHasta(nombre) {
-  window.location.href = "https://findea-web.s3.us-east-1.amazonaws.com/src/"+nombre;
+  window.location.href =
+    "https://findea-web.s3.us-east-1.amazonaws.com/src/" + nombre;
 }
-
